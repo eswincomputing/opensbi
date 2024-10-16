19f083805114015650b9bba686675a23a98b8f04
ningyu <ningyu@eswincomputing.com>
2024-10-16 09:42:17 +0800

    refactor:add version description
    Changelogs:
    Add mtimer synchronization mode description
    
    Signed-off-by: ningyu <ningyu@eswincomputing.com>

--- lib/sbi/sbi_init.c
+++ lib/sbi/sbi_init.c
@@ -45,8 +45,8 @@ static void sbi_boot_print_banner(struct sbi_scratch *scratch)
 #ifdef OPENSBI_VERSION_GIT
 	sbi_printf("\nOpenSBI %s\n", OPENSBI_VERSION_GIT);
 #else
-	sbi_printf("\nOpenSBI v%d.%d\n", OPENSBI_VERSION_MAJOR,
-		   OPENSBI_VERSION_MINOR);
+	sbi_printf("\nOpenSBI v%d.%d (%s)\n", OPENSBI_VERSION_MAJOR,
+		   OPENSBI_VERSION_MINOR, OPENSBI_VERSION_CUSTOM);
 #endif
 
 #ifdef OPENSBI_BUILD_TIME_STAMP
