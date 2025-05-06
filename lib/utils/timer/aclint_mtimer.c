8785b5253d349706b2720efc710b08139915f1da
ningyu <ningyu@eswincomputing.com>
2025-05-06 08:49:06 +0800

    fix:change to homologous crystal without mtimer sync
    Changelogs:
    Drop mtimer sync because d560 use homologous crystal oscillator default
    
    Signed-off-by: ningyu <ningyu@eswincomputing.com>

--- lib/utils/timer/aclint_mtimer.c
+++ lib/utils/timer/aclint_mtimer.c
@@ -19,7 +19,6 @@
 #include <sbi/riscv_locks.h>
 
 static unsigned long mtimer_ptr_offset;
-static spinlock_t mtime_lock = SPIN_LOCK_INITIALIZER;
 
 #define mtimer_get_hart_data_ptr(__scratch)				\
 	sbi_scratch_read_type((__scratch), void *, mtimer_ptr_offset)
@@ -62,36 +61,27 @@ static u64 mtimer_value(void)
 {
 	struct sbi_scratch *scratch = sbi_scratch_thishart_ptr();
 	struct aclint_mtimer_data *mt;
-	u64 d0, d1, val;
+	u32 target_hart = current_hartid();
+	u64 *time_val;
 
 	mt = mtimer_get_hart_data_ptr(scratch);
 	if (!mt)
 		return 0;
 
-// SOC with single die mode
-#if !defined(BR2_CHIPLET_1_DIE1_AVAILABLE) && defined(BR2_CHIPLET_1)
-	/* Read MTIMER Time Value */
-	return mt->time_rd((u64 *)((void *)mt->mtime_addr));
-#elif defined(BR2_CHIPLET_1_DIE1_AVAILABLE) && defined(BR2_CHIPLET_1)
-	return mt->time_rd((u64 *)((void *)mt->mtime_addr + 0x20000000));
+#if defined(BR2_PLATFORM_HAPS) && defined(BR2_CHIPLET_1_DIE1_AVAILABLE) && defined(BR2_CHIPLET_1)
+	if(target_hart >= 1  )
+#else
+	if(target_hart >= 4  )
 #endif
-
-// SOC with dual dies mode
-// sync with bigger value of mtime
-	spin_lock(&mtime_lock);
-	d0 = mt->time_rd((u64 *)((void *)mt->mtime_addr));
-	d1 = mt->time_rd((u64 *)((void *)mt->mtime_addr + 0x20000000));
-	if (d0 > d1) {
-		mt->time_wr(false, d0, (u64 *)((void *)mt->mtime_addr + 0x20000000));
-		val = d0;
-	} else if (d0 < d1) {
-		mt->time_wr(false, d1, (u64 *)((void *)mt->mtime_addr));
-		val = d1;
-	} else {
-		val = d0;
+	{
+		time_val = (void *)mt->mtime_addr + 0x20000000;
 	}
-	spin_unlock(&mtime_lock);
-	return val;
+	else
+	{
+		time_val = (void *)mt->mtime_addr;
+	}
+	/* Read MTIMER Time Value */
+	return mt->time_rd(time_val);
 }
 
 static void mtimer_event_stop(void)
